46f9c9677458b8a4f01573520e543522891d12fb
zwjtheone <474338731@qq.com>
2020-04-03 00:53:04 +0800

    进程学习
    Signed-off-by: zwjtheone <474338731@qq.com>

---
+++ 进程/child.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const server = http.createServer(function (req,res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('handle by child: pid is' + process.pid + '\n');
+
+})
+
+process.on('message', function (m, tcp) {
+  if (m === 'server'){
+    tcp.on('connection', function (socket) {
+      server.emit('connection', socket)
+    })
+  }
+
+})
